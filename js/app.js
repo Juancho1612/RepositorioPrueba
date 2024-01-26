158a62f4c8c6bffae13d79eb18ef9b8ac957f237
Juancho <juan.romero2011@gmail.com>
2024-01-26 14:59:49 -0300

    Agregar comentario:ejemplos los cambios

---
+++ js/app.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mi Primer Programa en JavaScript</title>
+</head>
+<body>
+
+  <h1>Mi Primer Programa en JavaScript</h1>
+
+  <script>
+    // Este es un comentario de una línea en JavaScript
+
+    // Declaración de una variable
+    var mensaje = "¡Hola, mundo!";
+
+    // Mostrar el mensaje en la consola del navegador
+    console.log(mensaje);
+
+    // Mostrar el mensaje en una ventana emergente (alert)
+    alert(mensaje);
+  </script>
+
+</body>
+</html>
